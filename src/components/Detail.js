227f5c80386814d0b8620e38b6cc73b83d174efc
Ezequiel Giussani <ezequielgiussani56@gmail.com>
2021-05-02 20:58:34 -0300

    ADDED: login

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -65,6 +65,7 @@ const ImageTitle = styled.div`
     width: 35vw;
     min-height: 170px;
     min-width: 200px;
+    martin-top: 60px;
 
     img {
         width: 100%;
@@ -137,5 +138,5 @@ const Description = styled.div`
     font-size: 20px;
     margin-top: 16px;
     color: rgb(249, 249, 249);
-    
+    max-width: 760px;
 `
